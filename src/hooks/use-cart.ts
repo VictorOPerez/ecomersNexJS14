19c7c877b772859059d115765574977f4b25599f
VictorOPerez <ortegaviktor571@gmail.com>
2023-12-07 00:56:16 -0500

    comit 2

--- src/hooks/use-cart.ts
+++ src/hooks/use-cart.ts
@@ -1,16 +1,9 @@
-// add items
-// remove items
-// clear the cart
-// (keep track of cart items)
-
-export type CartItem = {
-  product: Product;
-};
-
 import { Product } from "@/payload-types";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
-
+export type CartItem = {
+  product: Product;
+};
 type CartState = {
   items: CartItem[];
   addItem: (product: Product) => void;
